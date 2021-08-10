cb31595fbe1c6bd555c117e6efe4ce1ae0744370
a.akimov1 <reallex@yandex.ru>
2021-08-10 17:39:24 +0300

    Задание 2. Создана папочная структуа исходников. Скомпилировано приложение. Создан jar с классами

---
+++ src/MyApplication.java
@@ -0,0 +1,7 @@
+import com.tinkoff.edu.Printer;
+
+public class MyApplication {
+    public static void main(String[] args) {
+        Printer.print("Hello");
+    }
+}
---
+++ src/com/tinkoff/edu/Printer.java
@@ -0,0 +1,7 @@
+package com.tinkoff.edu;
+
+public class Printer {
+    public static void print(String message) {
+        System.out.println(message);
+    }
+}
