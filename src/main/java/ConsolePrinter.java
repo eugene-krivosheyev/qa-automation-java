fad6f6237447a58427405d91c4af723a32f7ff49
alexey-fmn <zooyork74@yandex.ru>
2022-03-23 16:04:44 +0300

    doc: add Quick Documentation to ConsolePrinter class

--- src/main/java/ConsolePrinter.java
+++ src/main/java/ConsolePrinter.java
@@ -1,4 +1,15 @@
+/**
+ * ConsolePrinter class prints variable at console
+ * <p>
+ * This method always print text at console
+ * Side effect -- text at console
+ *
+ * @author Alexey Fomin
+ */
 public class ConsolePrinter {
+    /**
+     * @param message -- text to be printed
+     */
     public static void print(String message) {
         System.out.println(message);
     }
