b9064c9931e8ca2f8fc57cd78f3e718187e583f6
k.m.averyanova <k.m.averyanova@tinkoff.ru>
2022-03-23 15:18:10 +0300

    Добавлены комментарии, добавлены директивы package, import

--- src/main/java/Application.java
+++
@@ -1,5 +0,0 @@
-class Application {
-    public static void main(String[] args) {
-        ConsolPrinter.print("Hello world!");
-    }
-}
---
+++ src/main/java/com/tcs/edu/printer/ConsolePrinter.java
@@ -0,0 +1,14 @@
+package com.tcs.edu.printer;
+/**
+ * Class is for printing message in console
+ * @author k.m.averyanova
+ */
+public class ConsolePrinter {
+    /**
+     * Method for printing message in console
+     * @param message message to print in console
+     */
+    public static void print(String message) {
+        System.out.println(message);
+    }
+}
