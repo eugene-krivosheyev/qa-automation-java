2c25f414078d0cf5f1d845630b5f59e07789a705
alexey-fmn <zooyork74@yandex.ru>
2022-03-25 09:42:22 +0300

    feat: add decorate func

--- src/main/java/com/tcs/edu/Application.java
+++ src/main/java/com/tcs/edu/Application.java
@@ -1,9 +1,9 @@
 package com.tcs.edu;
 
-import com.tcs.edu.printer.ConsolePrinter;
+import static com.tcs.edu.decorator.TimestampMessageDecorator.decorate;
 
 class Application {
     public static void main(String[] args) {
-        ConsolePrinter.print("Hello world!");
+        System.out.println(decorate("Hello world!"));
     }
 }
--- src/main/java/com/tcs/edu/printer/ConsolePrinter.java
+++ src/main/java/com/tcs/edu/printer/ConsolePrinter.java
@@ -3,15 +3,15 @@ package com.tcs.edu.printer;
 /**
  * ConsolePrinter class prints variable at console
  * <p>
- * This method always print text at console
+ * This class always print text at console
  *
  * @author Alexey Fomin
  */
 public class ConsolePrinter {
     /**
-     * @param message -- text to be printed
+     * @param message -- text to print
      */
-    public static void print(String message) {
+    public static void print(String message){
         System.out.println(message);
     }
 }
