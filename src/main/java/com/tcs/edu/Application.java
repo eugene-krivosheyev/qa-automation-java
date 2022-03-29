9c0d17630bd1da3484aa97b00eec9ed8998e7104
z.starokozhev <zacharysato@hotmail.com>
2022-03-29 23:13:21 +0300

    feat: add global counter to TimestampMessageDecorator

--- src/main/java/com/tcs/edu/Application.java
+++ src/main/java/com/tcs/edu/Application.java
@@ -6,5 +6,7 @@ import com.tcs.edu.printer.ConsolePrinter;
 class Application {
     public static void main(String[] args) {
         ConsolePrinter.print(TimestampMessageDecorator.decorate("Hello world!"));
+        ConsolePrinter.print(TimestampMessageDecorator.decorate("Hello world!"));
+        ConsolePrinter.print(TimestampMessageDecorator.decorate("Hello world!"));
     }
 }
--- src/main/java/com/tcs/edu/decorator/TimestampMessageDecorator.java
+++ src/main/java/com/tcs/edu/decorator/TimestampMessageDecorator.java
@@ -9,13 +9,19 @@ import static java.time.Instant.now;
  */
 public class TimestampMessageDecorator {
     /**
+     * <code>messageCount</code> stores the number of {@link #decorate(String)} calls.
+     */
+    private static int messageCount;
+    /**
      * Adds a space-delimited timestamp to the beginning of the passed string using concatenation.
+     * Side effect on global <code>messageCount</code> - increment
      * @param message The <code>String</code> to be decorated
      * @return The <code>String</code> decorated with a timestamp
      */
     public static String decorate(String message){
         String decoratedMessage;
-        decoratedMessage = now() + " " + message;
+        messageCount++;
+        decoratedMessage = messageCount + " " + now() + " " + message;
         return decoratedMessage;
     }
 }
