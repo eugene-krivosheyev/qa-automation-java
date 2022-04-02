f4150ab5a7f9ba3e26497c7f59a560dee556a41e
Ochilova Alisa Aleksandrovna <a.ochilova@tinkoff.ru>
2022-04-02 20:32:56 +0300

    feat: Тренировка #4.1-4.4, в методе decorator  введена поясняющая переменная decoratedMessage, добавлена общая переменная messageCount с количеством выведенных сообщений, увеличено количество выводов сообщения

--- src/main/java/com/tcs/edu/Application.java
+++ src/main/java/com/tcs/edu/Application.java
@@ -1,10 +1,12 @@
 package com.tcs.edu;
 
-import static  com.tcs.edu.decorator.TimestampMessageDecorator.decorator;
+import static com.tcs.edu.decorator.TimestampMessageDecorator.*;
 import static com.tcs.edu.printer.ConsolePrinter.print;
 
 class Application {
     public static void main(String[] args) {
+        print(decorator("Hello world!"));
+        print(decorator("Hello world!"));
         print(decorator("Hello world!"));
     }
 }
--- src/main/java/com/tcs/edu/decorator/TimestampMessageDecorator.java
+++ src/main/java/com/tcs/edu/decorator/TimestampMessageDecorator.java
@@ -3,7 +3,11 @@ package com.tcs.edu.decorator;
 import  java.time.Instant;
 
 public class TimestampMessageDecorator {
+
+    public static int messageCount;
+
     public static String decorator(String message){
-        return Instant.now() + " " + message;
+        final var decoratedMessage = ++messageCount + " " + Instant.now() + " " + message;
+        return decoratedMessage;
     }
 }
