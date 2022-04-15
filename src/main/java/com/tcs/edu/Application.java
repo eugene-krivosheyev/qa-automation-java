4752cc5e10892150442db81f377b0e9d7bbce419
k.m.averyanova <k.m.averyanova@tinkoff.ru>
2022-04-15 19:51:20 +0300

    Реализован процессинг vararg c помощью цикла foreach

--- src/main/java/com/tcs/edu/Application.java
+++ src/main/java/com/tcs/edu/Application.java
@@ -1,11 +1,12 @@
 package com.tcs.edu;
+import com.tcs.edu.decorator.MessageService;
 import com.tcs.edu.decorator.Severity;
 import com.tcs.edu.decorator.TimestampMessageDecorator;
 import com.tcs.edu.printer.ConsolePrinter;
 class Application {
     public static void main(String[] args) {
-        ConsolePrinter.print(TimestampMessageDecorator.decorate( Severity.MINOR, "Hello world!"));
-        ConsolePrinter.print(TimestampMessageDecorator.decorate(Severity.REGULAR, "Hello world!"));
-        ConsolePrinter.print(TimestampMessageDecorator.decorate(Severity.MAJOR, "Hello world!"));
+        MessageService.print(Severity.MINOR, "Hello world!");
+        MessageService.print(Severity.REGULAR, "Hello world!");
+        MessageService.print(Severity.MAJOR, "Hello world!");
     }
 }
--- src/main/java/com/tcs/edu/decorator/MessageService.java
+++ src/main/java/com/tcs/edu/decorator/MessageService.java
@@ -1,5 +1,7 @@
 package com.tcs.edu.decorator;
 
+import com.tcs.edu.printer.ConsolePrinter;
+
 import java.time.Instant;
 
 public class MessageService {
@@ -19,5 +21,11 @@ public class MessageService {
 
     public static String concatenate(Severity severity, int messageCount, String message) {
         return String.format("%d %s %s %s", messageCount, Instant.now(), message, severityValue(severity));
+    }
+
+    public static void print(Severity severity, String ...messages) {
+        for (String message : messages) {
+            ConsolePrinter.print(TimestampMessageDecorator.decorate(severity, message));
+        }
     }
 }
