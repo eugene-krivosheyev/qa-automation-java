49d9b2b50fbe6c5d458748d83ff1c55ecdb10461
z.starokozhev <zacharysato@hotmail.com>
2022-04-16 15:59:57 +0300

    refactor: split service to printing and processing methods

--- src/main/java/com/tcs/edu/Application.java
+++ src/main/java/com/tcs/edu/Application.java
@@ -1,14 +1,12 @@
 package com.tcs.edu;
 
-import static com.tcs.edu.service.MessageService.process;
+import static com.tcs.edu.service.MessageService.print;
 import static com.tcs.edu.decorator.Severity.*;
 
 class Application {
     public static void main(String[] args) {
-        process(MAJOR, "Greetings!");
-        process(REGULAR, "Nihao!", "Hey!");
-        process(MINOR, "Hello!", "Hi!", "Howdy!");
-        //не выводит ничего
-        process(MAJOR);
+        print(MAJOR, "Greetings!");
+        print(REGULAR, "Nihao!", "Hey!");
+        print(MINOR, "Hello!", "Hi!", "Howdy!");
     }
 }
--- src/main/java/com/tcs/edu/service/MessageService.java
+++ src/main/java/com/tcs/edu/service/MessageService.java
@@ -4,8 +4,7 @@ import com.tcs.edu.decorator.PaginationDecorator;
 import com.tcs.edu.decorator.Severity;
 import com.tcs.edu.decorator.SeverityMessageDecorator;
 import com.tcs.edu.decorator.TimestampMessageDecorator;
-
-import static com.tcs.edu.printer.ConsolePrinter.print;
+import com.tcs.edu.printer.ConsolePrinter;
 
 public class MessageService {
     /**
@@ -18,23 +17,35 @@ public class MessageService {
     private static final int PAGE_SIZE = 2;
 
     /**
+     * Side effect on global {@link #messageCount} increases depending on the number of rows passed
+     *
+     * @param severity A parameter to denote the implication and the impact of the defect on the functionality
+     * @param message  <code>String</code> to be proceeded. Disables calling without messages.
+     * @param messages <code>Strings</code> to be proceeded to print.
+     */
+    public static void print(final Severity severity, final String message, final String... messages) {
+        process(severity, message);
+        for (String currentMessage : messages) {
+            process(severity, currentMessage);
+        }
+    }
+
+    /**
      * Side effect on global {@link #messageCount} - increment.
      * Glues a decorated string with an ordinal number.
      * Includes pagination. The page size is determined by {@link #PAGE_SIZE}.
      * Prints out the result.
      *
-     * @param severity A parameter to denote the implication and the impact of the defect on the functionality
-     * @param messages <code>Strings</code> to be proceeded to print
+     * @param severity       A parameter to denote the implication and the impact of the defect on the functionality
+     * @param currentMessage <code>String</code> to be proceeded to print
      */
-    public static void process(Severity severity, String... messages) {
-        for (String message : messages) {
-            messageCount++;
-            message = TimestampMessageDecorator.decorate(message);
-            message = SeverityMessageDecorator.decorate(severity, message);
-            if (messageCount % PAGE_SIZE == 0) {
-                message = PaginationDecorator.decorate(message);
-            }
-            print(messageCount + " " + message);
+    private static void process(final Severity severity, String currentMessage) {
+        messageCount++;
+        currentMessage = TimestampMessageDecorator.decorate(currentMessage);
+        currentMessage = SeverityMessageDecorator.decorate(severity, currentMessage);
+        if (messageCount % PAGE_SIZE == 0) {
+            currentMessage = PaginationDecorator.decorate(currentMessage);
         }
+        ConsolePrinter.print(messageCount + " " + currentMessage);
     }
 }
