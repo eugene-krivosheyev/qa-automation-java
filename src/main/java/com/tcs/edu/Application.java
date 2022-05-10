cadb5590cfc68f61b7ab253c1966e7b8295d6709
k.m.averyanova <k.m.averyanova@tinkoff.ru>
2022-05-10 20:28:20 +0300

    Добавлен класс Message

--- src/main/java/com/tcs/edu/Application.java
+++ src/main/java/com/tcs/edu/Application.java
@@ -1,10 +1,18 @@
 package com.tcs.edu;
 import com.tcs.edu.decorator.*;
+import com.tcs.edu.domain.Message;
 import com.tcs.edu.printer.ConsolePrinter;
 class Application {
     public static void main(String[] args) {
-        MessageService.print(Severity.MINOR, MessageOrder.ASC,  Doubling.DISTINCT, "Hello world! 0", "Hello world! 0", "Hello world! 1", "Hello again! 2", null, "Hello world! 3");
-        MessageService.print(Severity.REGULAR, "Hello world!");
-        MessageService.print(Severity.MAJOR, "Hello world!");
+        MessageService.print(MessageOrder.ASC,
+                Doubling.DISTINCT,
+                new Message("Hello world! 0", Severity.MINOR),
+                new Message("Hello world! 0", Severity.MINOR),
+                new Message("Hello world! 1", Severity.MINOR),
+                new Message("Hello world! 2", Severity.MINOR),
+                null,
+                new Message("Hello world! 3", Severity.MINOR),
+                new Message("Hello world!", Severity.REGULAR),
+                new Message("Hello world!", Severity.MAJOR));
     }
 }
---
+++ src/main/java/com/tcs/edu/domain/Message.java
@@ -0,0 +1,29 @@
+package com.tcs.edu.domain;
+
+import com.tcs.edu.decorator.Severity;
+
+public class Message {
+    String body;
+    Severity severity;
+
+    public Message(String body, Severity severity) {
+        this.body = body;
+        this.severity = severity;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public Severity getSeverity() {
+        return severity;
+    }
+
+    public void setSeverity(Severity severity) {
+        this.severity = severity;
+    }
+}
