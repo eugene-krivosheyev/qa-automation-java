ed60a9c661e26d8f0be0a5ae98ba0835469de831
z.starokozhev <zacharysato@hotmail.com>
2022-05-29 20:32:57 +0300

    feat: change tests

--- src/main/java/com/tcs/edu/Application.java
+++ src/main/java/com/tcs/edu/Application.java
@@ -8,6 +8,8 @@ import com.tcs.edu.repository.MessageRepository;
 import com.tcs.edu.service.MessageService;
 import com.tcs.edu.service.OrderedDistinctedMessageService;
 
+import java.util.UUID;
+
 import static com.tcs.edu.decorator.Severity.*;
 
 class Application {
@@ -28,13 +30,12 @@ class Application {
             e.printStackTrace();
         }
         System.out.println("Сохраненные сообщения:");
-        service.findAll().forEach(message -> {
-            System.out.println("Сообщение просто так: " + message);
-            System.out.println("Сообщение по ключу: "
-                    + message.getId() + " -> "
-                    + service.findByPrimaryKey(message.getId()));
-        });
+        service.findAll().forEach(System.out::println);
         System.out.println("Сообщения только низкой критичности:");
         service.findAllBySeverity(MINOR).forEach(System.out::println);
+        System.out.println("Сообщение по ключу, например:");
+        UUID key = service.findAll().stream().
+                filter(message -> message.getSeverity() == MAJOR).findFirst().get().getId();
+        System.out.println(key + " -> " + service.findByPrimaryKey(key));
     }
 }
